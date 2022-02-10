140d3aad3ed82ad9ba851afe9a58d2a23b1bb8eb
Struchkov Mark <git@upagge.ru>
2022-02-10 22:27:03 +0300

    Добавил Supplier для исключения FilterJooqHaitiException

--- src/main/java/dev/struchkov/haiti/filter/jooq/exception/FilterJooqHaitiException.java
+++ src/main/java/dev/struchkov/haiti/filter/jooq/exception/FilterJooqHaitiException.java
@@ -1,20 +1,18 @@
 package dev.struchkov.haiti.filter.jooq.exception;
 
-import dev.struchkov.haiti.filter.exception.FilterException;
+import dev.struchkov.haiti.context.exception.BasicException;
 
 import java.text.MessageFormat;
 import java.util.function.Supplier;
 
-public class FilterJooqHaitiException extends FilterException {
+public class FilterJooqHaitiException extends BasicException {
 
     public FilterJooqHaitiException(String message) {
         super(message);
     }
 
     public static Supplier<FilterJooqHaitiException> filterJooqException(String message, Object... objects) {
-        return () -> {
-            return new FilterJooqHaitiException(MessageFormat.format(message, objects));
-        };
+        return () -> new FilterJooqHaitiException(MessageFormat.format(message, objects));
     }
 
 }
